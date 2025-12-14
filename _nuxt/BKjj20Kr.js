2736da864fe221df1acb931b39c47b83995a6be4
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2025-12-14 20:10:42 +0100

    Updates

---
+++ _nuxt/BKjj20Kr.js
@@ -0,0 +1,1 @@
+import{r as e}from"./Dlc6hVft.js";import n from"./4tHzR_vS.js";import{S as s,c as t,o as a,w as c,u as i}from"./CfswjZTr.js";const l={__name:"ProseNote",setup(p){const o=s();return(r,f)=>(a(),t(n,{color:"info",icon:i(o).ui.icons.info},{default:c(()=>[e(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
