96d30f683eff795de8348eae90b1a5a498037d06
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2025-12-15 08:44:22 +0100

    Updates

--- _nuxt/Duc17Zkq.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./BdKBmZrl.js";import e from"./0WjESwWy.js";import{V as t,c as a,o as c,w as n,u as p}from"./BE0zz3Wo.js";const l={__name:"ProseTip",setup(i){const o=t();return(s,f)=>(c(),a(e,{color:"success",icon:p(o).ui.icons.tip},{default:n(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
