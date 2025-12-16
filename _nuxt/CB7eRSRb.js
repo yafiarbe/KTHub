c3bd1838a2aa7f8b4b9e60f0b6ffb0e008a3cc3b
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2025-12-16 21:47:36 +0100

    Updates

---
+++ _nuxt/CB7eRSRb.js
@@ -0,0 +1,1 @@
+import{r as e}from"./B0jKdGDa.js";import n from"./C6bGzSDh.js";import{V as s,c as t,o as a,w as c,u as i}from"./dsDpxm9I.js";const l={__name:"ProseNote",setup(p){const o=s();return(r,f)=>(a(),t(n,{color:"info",icon:i(o).ui.icons.info},{default:c(()=>[e(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
--- _nuxt/Dzr4YJiN.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./Dv5mDUaD.js";import e from"./CuzXutud.js";import{V as t,c as a,o as c,w as n,u as p}from"./lW9KsipQ.js";const l={__name:"ProseTip",setup(i){const o=t();return(s,f)=>(c(),a(e,{color:"success",icon:p(o).ui.icons.tip},{default:n(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
