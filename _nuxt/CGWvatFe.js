99b0361cfbf573007491d3deae782569872419d3
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2025-12-16 21:51:51 +0100

    Updates

---
+++ _nuxt/CGWvatFe.js
@@ -0,0 +1,1 @@
+import{r}from"./f3JGX9eZ.js";import e from"./DLKEGb74.js";import{V as t,c as a,o as c,w as n,u as p}from"./Ba79fHjy.js";const l={__name:"ProseTip",setup(i){const o=t();return(s,f)=>(c(),a(e,{color:"success",icon:p(o).ui.icons.tip},{default:n(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
---
+++ _nuxt/DBZx2FJl.js
@@ -0,0 +1,1 @@
+import{r as a}from"./f3JGX9eZ.js";import t from"./DLKEGb74.js";import{V as e,c as n,o as s,w as c,u as i}from"./Ba79fHjy.js";const l={__name:"ProseCaution",setup(p){const o=e();return(r,u)=>(s(),n(t,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
